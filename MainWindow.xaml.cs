561b27ba6f4449a66e368d79339c4b77a9ec0e69
DisabledMallis <mallisiscool@gmail.com>
2019-10-08 23:24:55 -0400

    Aggiungere i file di progetto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilePatcher
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileInfo toPatch;
+        FileInfo patch;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            if (ofd.ShowDialog() == true)
+            {
+                toPatch = new FileInfo(ofd.FileName);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "FP files (*.fp)|*.fp|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == true)
+            {
+                patch = new FileInfo(ofd.FileName);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if(toPatch == null)
+            {
+                MessageBox.Show("Please select a file to patch!");
+                return;
+            }
+            if (patch == null)
+            {
+                MessageBox.Show("Please select a patch!");
+                return;
+            }
+            //get offset from file
+            string data = File.ReadLines(patch.FullName).ToList()[0];
+            string offset = Tokens.getData(data, "Offset");
+            progBar.Value += 1;
+
+            //get patch data from file
+            data = File.ReadLines(patch.FullName).ToList()[1];
+            string patchedData = Tokens.getData(data, "Patch");
+            string[] patchedDataArr = patchedData.Split(' ');
+            List<byte> patchBytes = new List<byte>();
+            foreach(string byteStr in patchedDataArr)
+            {
+                patchBytes.Add(Convert.ToByte(byteStr, 16));
+            }
+            progBar.Value += 1;
+
+            using (Stream stream = File.Open(toPatch.FullName, FileMode.Open))
+            {
+                stream.Position = Convert.ToInt64(offset, 16);
+                stream.Write(patchBytes.ToArray(), 0, patchBytes.Count);
+                progBar.Value += 1;
+            }
+
+            MessageBox.Show("Patch complete!");
+        }
+    }
+}
---
+++ Tokens.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FilePatcher
+{
+    class Tokens
+    {
+        public static string getData(string data, string getType)
+        {
+            bool boolType = false;
+            bool getData = false;
+            string type = "";
+            string datas = "";
+            foreach (char c in data)
+            {
+                if (c == '{')
+                {
+                    boolType = true;
+                    continue;
+                }
+                if (c == '}')
+                {
+                    boolType = false;
+                    continue;
+                }
+                if (c == '[')
+                {
+                    getData = true;
+                    continue;
+                }
+                if (c == ']')
+                {
+                    getData = false;
+                    continue;
+                }
+                if (boolType) {
+                    type += c;
+                }
+                if (getData)
+                {
+                    datas += c;
+                }
+            }
+            if (type == getType)
+            {
+                return datas;
+            }
+            return datas;
+        }
+    }
+}
